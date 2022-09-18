d769fae93e641d172fcf5b7366cd0671903346fc
zexhozou <zexhozou@tencent.com>
2022-09-18 23:12:33 +0800

    init

---
+++ src/test/java/com/hateoas/hateoasdemo/HateoasDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hateoas.hateoasdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HateoasDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
