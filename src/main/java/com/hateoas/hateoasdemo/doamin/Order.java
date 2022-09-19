744b16351b6db0f85ec278f969ed5bb84306a372
zexhozou <zexhozou@tencent.com>
2022-09-19 19:05:00 +0800

    feat: 添加 Order Bo

---
+++ src/main/java/com/hateoas/hateoasdemo/doamin/Order.java
@@ -0,0 +1,36 @@
+package com.hateoas.hateoasdemo.doamin;
+
+import org.springframework.hateoas.EntityModel;
+
+public class Order extends EntityModel<Order> {
+
+    private String orderId;
+
+    private Double price;
+
+    private Integer quantity;
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
